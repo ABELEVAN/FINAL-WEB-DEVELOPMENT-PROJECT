1fc98eb8afea043f9752370e1ce1c1e8d5455401
ABELEVAN <abelevan21@gmail.com>
2024-09-30 03:00:49 +0300

    update on response to users without token when trying to add food listings

--- server/routes/foodRoutes.js
+++ server/routes/foodRoutes.js
@@ -9,10 +9,10 @@ const router = express.Router();
 // Middleware to verify token
 function verifyToken(req, res, next) {
     const token = req.headers['authorization'];
-    if (!token) return res.status(403).send('Token required.');
+    if (!token) return res.status(403).send('Token required. Please log in and try again.');
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) return res.status(401).send('Invalid token.');
+        if (err) return res.status(401).send('Invalid token. Please log in and try again.');
         req.userId = decoded.id;
         next();
     });
